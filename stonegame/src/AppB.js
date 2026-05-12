25b3b90a4bf16aa48ddee34a50faae027d2d1191
selwjoo <moi080917@gmail.com>
2026-05-12 14:56:54 +0900

    야르

--- stonegame/src/AppB.js
+++ stonegame/src/AppB.js
@@ -1,10 +1,11 @@
 import { useState, useRef } from "react"
 
+
 export default function App() {
   const [money, setMoney] = useState(0)  // 시작 돈은 0원
   const lastClickTime = useRef(0) // 마지막 클릭 시간은 화면에 표시할거 아니니까 기억만 하면 되서 useState가 아니라 useRef임. 유가릿? 
-
   const [combo, setCombo] = useState(1)       // 콤보 배율 (1, 2, 3, 4...)
+  
 
   function handleClick() {
     const now = Date.now()
@@ -23,13 +24,15 @@ export default function App() {
     setMoney(money + newCombo)      // 콤보 배율 적용!
   }
 
-
+  
 
   return (
     <div>
       <h1>💰 {money}원</h1>
       <button onClick={handleClick}>클릭해서 돈 벌기</button>
+
     </div>
+
   )
 }
 
