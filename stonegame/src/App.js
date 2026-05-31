b3f42a288646ea878a6ebfbfaca0ebc83bfb4fb3
jyk864 <jyk864@gmail.com>
2026-05-31 11:00:40 +0900

    ㄹ호

--- stonegame/src/App.js
+++ stonegame/src/App.js
@@ -17,7 +17,7 @@ function App() {
   const [gameOver, setGameOver] = useState(false); // 게임 종료시 true!! // 포션 구매 시, 다시 false로 변경
   const [message, setMessage] = useState(''); // 게임 종료 후 포션 구매 or 포기 선택하는 메세지 창 팝업
 
-  const [potionPrice, setPotionPrice] = useState(50); // 포션 가격
+  const [potionPrice, setPotionPrice] = useState(50000); // 포션 가격
   const [reviveCount, setReviveCount] = useState(0); 
   // 부활 횟수..! 초기에 0으로 선언하여 (부활횟수) x (포션 가격) x 3 하여 포션 가격을 업데이트 후, 부활 횟수에 1을 더하는 형태
 
